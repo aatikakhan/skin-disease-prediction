f6ddedc83911f244dca1ca39ddf9f9e8e1d71978
aatikakhan <aatikakhan869@gmail.com>
2025-09-25 03:35:12 +0530

    add CROS issue catch

--- backend/main.py
+++ backend/main.py
@@ -8,6 +8,16 @@ import torch.nn as nn
 
 app = FastAPI()
 
+from fastapi.middleware.cors import CORSMiddleware
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 # Device
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
